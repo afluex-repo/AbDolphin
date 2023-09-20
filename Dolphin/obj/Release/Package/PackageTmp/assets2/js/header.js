4191e42b723c653e3fd570345a7d7e7a72ffcb2b
afluex-sudhakar <79094025+afluex-sudhakar@users.noreply.github.com>
2023-09-20 17:08:45 +0530

    changes

---
+++ Dolphin/obj/Release/Package/PackageTmp/assets2/js/header.js
@@ -0,0 +1,98 @@
+document.write(`
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Abdolphin -Real Estate</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+    <!--External CSS libraries-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-submenu.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="css/leaflet.css" type="text/css">
+    <link rel="stylesheet" href="css/map.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="fonts/linearicons/style.css">
+    <link rel="stylesheet" type="text/css"  href="css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" type="text/css"  href="css/magnific-popup.css">
+    <link rel="stylesheet" type="text/css"  href="css/dropzone.css">
+    <link rel="stylesheet" type="text/css"  href="css/slick.css">
+
+    <!--Custom stylesheet-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" id="style_sheet" href="css/skins/default.css">
+
+    <!--Favicon icon-->
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" >
+
+    <!--Google fonts-->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CRoboto:300,400,500,700&amp;display=swap">
+
+    <!--IE10 viewport hack for Surface/desktop Windows 8 bug-->
+    <link rel="stylesheet" type="text/css" href="css/ie10-viewport-bug-workaround.css">
+
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+</head>
+<body>
+<div class ="page_loader"></div>
+
+<!--Main header start-->
+<header class ="main-header mh-3 header-transparent">
+    <div class ="container-fluid" style="padding: 0 10%;">
+        <nav class ="navbar navbar-expand-lg navbar-light">
+            <a class ="navbar-brand logos" href="index.html">
+                <img src="img/logos/logo.png" alt="logo" class ="logo-none-2">
+                <img src="img/logos/logo.png" alt="logo" class ="logo-none">
+            </a>
+            <button class ="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class ="navbar-toggler-icon"></span>
+            </button>
+            <div class ="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class ="navbar-nav ml-auto">
+                    <li class ="nav-item active">
+                        <a class ="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class ="nav-item dropdown">
+                        <a class ="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Us</a>
+                        <ul class ="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li class =""><a class ="dropdown-item" href="CompanyOverview.html">Company Overview</a></li>
+                            <li class =""><a class ="dropdown-item" href="team.html">Our Team</a></li>
+                            <li class =""><a class ="dropdown-item" href="VisionAndMission.html">Vision & Mission</a></li>
+                            <li class =""><a class ="dropdown-item" href="qualitypolicy.html">Quality Policy</a></li>
+                        </ul>
+                    </li>
+                    <li class ="nav-item">
+                        <a class ="nav-link" href="http://www.abdolphininfratech.com/Website/UnitAvailability">Unit Availability</a>
+                    </li>
+                    <li class ="nav-item dropdown">
+                        <a class ="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Maps</a>
+                        <ul class ="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li class =""><a class ="dropdown-item" href="img/project-map/Dolphin-smartcity.pdf" target="_blank">Map 1 Deoria</a></li>
+                            <li class =""><a class ="dropdown-item" href="img/project-map/gorakhpur-Model.pdf" target="_blank">Map 2 Gorakhpur</a></li>
+                            <li class =""><a class ="dropdown-item" href="img/project-map/green city new Final.pdf" target="_blank">Map 3 Maniram</a></li>
+                        </ul>
+                    </li>
+                    <li class ="nav-item">
+                        <a class ="nav-link" href="gallery.html">Gallery</a>
+                    </li>
+                    <li class ="nav-item">
+                        <a class ="nav-link" href="contact.html">Contact Us</a>
+                    </li>
+                    <!-- <li class ="nav-item login-ml">
+                        <a class ="btn btn-sm btn-block-outline signup-link" href="login.html">Login</a>
+                    </li> -->
+                    <li class ="nav-item">
+                        <a class ="btn btn-sm btn-block  signup-link hidden-mb-20" href="login.html">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
+`);
